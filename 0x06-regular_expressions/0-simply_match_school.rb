01e087c2fb76fe53b3d931c2aa34c11cd5e43740
damtrix <oludamola.onarinde@gmail.com>
2023-01-31 04:43:09 -0800

    update

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# The regular expression that will match school
-puts ARVG[0].scan(/School/).join
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
