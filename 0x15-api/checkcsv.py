beefb76603785c71597f9140e391b1472de29740
damtrix <oludamola.onarinde@gmail.com>
2023-04-24 13:47:38 +0100

    all

--- 0x15-api/checkcsv.py
+++ 0x15-api/checkcsv.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
+"""
+Check student .CSV output of user information
+"""
 
+import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/todos"
-    
-    
-    response = requests.get(url).json()
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+
+def user_info(id):
+    """ Check user information """
+
     total_tasks = 0
+    response = requests.get(todos_url).json()
     for i in response:
-        if i['userId'] == int(id):
+        if i['userId'] == id:
             total_tasks += 1
-    
-   
+
     num_lines = 0
     with open(str(id) + ".csv", 'r') as f:
         for line in f:
             if not line == '\n':
                 num_lines += 1
-                
-    print("num_lines: {}".format(num_lines))
-    print("Total_tasks: {}".format(total_tasks))
+
+    if total_tasks == num_lines:
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
+
+
+if __name__ == "__main__":
+    user_info(int(sys.argv[1]))
